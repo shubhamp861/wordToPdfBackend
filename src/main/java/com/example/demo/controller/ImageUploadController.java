a99220fd2b48adb59f64b8fbceeeb3d32d12dc9a
shubhamp861 <sp7075892@gmail.com>
2021-01-11 06:33:55 -0800

    resource folder

--- src/main/java/com/example/demo/controller/ImageUploadController.java
+++ src/main/java/com/example/demo/controller/ImageUploadController.java
@@ -35,40 +35,38 @@ public class ImageUploadController {
 		}
 	}
 
-	@GetMapping("/abc")
-	@CrossOrigin
-	public ResponseEntity<byte[]> get() throws Exception{
-		String  propertyFile = System.getProperty("user.dir")+"\\src\\main\\resources\\";
-		File file =  new File(propertyFile+"fil.docx");
-		InputStream in = new BufferedInputStream(new FileInputStream(file));
-		int u =(int) file.length();
-		//byte[] buf = new byte[u];
-
-		List<String> employees = Arrays.asList("hello","bye");
-		ObjectMapper objectMapper = new ObjectMapper();
-		String json = objectMapper.writeValueAsString(employees);
-		byte[] buf = json.getBytes();
-
-		String fileName = "sample.pdf";
-		HttpHeaders respHeaders = new HttpHeaders();
-		respHeaders.setContentLength(buf.length);
-		respHeaders.setContentType(new MediaType("text", "json"));
-		respHeaders.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-		respHeaders.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
-		System.out.println("called");
-		return new ResponseEntity<byte[]>(buf, respHeaders, HttpStatus.OK);
-
-	}
+//	@GetMapping("/abc")
+//	@CrossOrigin
+//	public ResponseEntity<byte[]> get() throws Exception{
+//		String  propertyFile = System.getProperty("user.dir")+"\\src\\main\\resources\\";
+//		File file =  new File(propertyFile+"fil.docx");
+//		InputStream in = new BufferedInputStream(new FileInputStream(file));
+//		int u =(int) file.length();
+//		//byte[] buf = new byte[u];
+//
+//		List<String> employees = Arrays.asList("hello","bye");
+//		ObjectMapper objectMapper = new ObjectMapper();
+//		String json = objectMapper.writeValueAsString(employees);
+//		byte[] buf = json.getBytes();
+//
+//		String fileName = "sample.pdf";
+//		HttpHeaders respHeaders = new HttpHeaders();
+//		respHeaders.setContentLength(buf.length);
+//		respHeaders.setContentType(new MediaType("text", "json"));
+//		respHeaders.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+//		respHeaders.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
+//		System.out.println("called");
+//		return new ResponseEntity<byte[]>(buf, respHeaders, HttpStatus.OK);
+//
+//	}
 	@GetMapping("/a")
 	@CrossOrigin
 	public Object getpdf() throws Exception{
 		String  propertyFile = System.getProperty("user.dir")+"\\src\\main\\resources\\";
 		File file =  new File(propertyFile+"Sample.pdf");
 		FileInputStream fileInputStream = new FileInputStream(file);
-		System.out.println(IOUtils.toByteArray(fileInputStream));
 		return IOUtils.toByteArray(fileInputStream);
 	}
-	
 	@GetMapping("/")
 	@CrossOrigin
 	public List<String> hello(){
